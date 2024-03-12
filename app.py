a7a66f7d9940361e71ddeb21eb02900b8b00c537
paragdessai <paragdesai123@gmail.com>
2024-03-12 16:26:13 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, flash
+app = Flask(__name__)
+app.secret_key = "Parag123"
+
+@app.route("/hello")
+def index():
+    flash("whats your name ?")
+    return render_template("index.html")
+
+@app.route("/greet", methods = ["POST","GET"])
+def greet():
+    flash("Hi " + str(request.form['name_input']) + ", great to see you")
+    request.form['name_input']
+    return render_template("index.html")
